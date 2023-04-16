8a198cfda3e2b972afc62665ffcdb005994bc7c9
gjescuderoamodeo <49474070+gjescuderoamodeo@users.noreply.github.com>
2023-04-16 12:23:37 +0200

    add login screen and dependencies

---
+++ lib/services/services.dart
@@ -0,0 +1,2 @@
+export 'package:tourboost_app/services/auth_service.dart';
+export 'package:tourboost_app/services/notifications_service.dart';
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,8 +1,6 @@
-import 'package:tourboost_app/widgets/custom_bottom_navigation.dart';
-
-export 'package:tourboost_app/widgets/background.dart';
 export 'package:tourboost_app/widgets/custom_bottom_navigation.dart';
+export 'package:tourboost_app/widgets/background.dart';
 export 'package:tourboost_app/widgets/page_title.dart';
 export 'package:tourboost_app/widgets/card_table.dart';
-
-
+export 'package:tourboost_app/widgets/auth_background.dart';
+export 'package:tourboost_app/widgets/card_container.dart';
