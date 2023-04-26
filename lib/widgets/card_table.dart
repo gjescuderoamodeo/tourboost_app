ea030d280a7cba2f523ebca28aba1c67ff0104b5
gjescuderoamodeo <49474070+gjescuderoamodeo@users.noreply.github.com>
2023-04-26 12:33:42 +0200

    Update card_table.dart

--- lib/widgets/card_table.dart
+++ lib/widgets/card_table.dart
@@ -7,83 +7,109 @@ class CardTable extends StatelessWidget {
   Widget build(BuildContext context) {
     return Table(
       //opciones disponible app
-      children: const [
-        TableRow(
-          children: [
-            _SigleCard( color: Color.fromARGB(255, 34, 188, 85), icon: Icons.map_sharp, text: 'Mapa' ),
-            _SigleCard( color: Colors.pinkAccent, icon: Icons.hotel_sharp, text: 'Hoteles' ),
-          ]
-        ), 
-        TableRow(
-          children: [
-            _SigleCard( color: Color.fromARGB(255, 203, 186, 0), icon: Icons.star_purple500_outlined, text: 'Marcadores favoritos' ),
-            _SigleCard( color: Colors.pinkAccent, icon: Icons.hotel_rounded, text: 'Mis reservas hotel' ),
-          ]
-        ),
+      children: [
+        TableRow(children: [
+          GestureDetector(
+            onTap: () {
+              Navigator.pushNamed(context, 'map');
+            },
+            child: const _SigleCard(
+              color: Color.fromARGB(255, 34, 188, 85),
+              icon: Icons.map_sharp,
+              text: 'Mapa',
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.pushNamed(context, 'map');
+            },
+            child: const _SigleCard(
+              color: Colors.pinkAccent,
+              icon: Icons.hotel_sharp,
+              text: 'Hoteles',
+            ),
+          ),
+        ]),
+        TableRow(children: [
+          GestureDetector(
+            onTap: () {
+              Navigator.pushNamed(context, 'map');
+            },
+            child: const _SigleCard(
+              color: Color.fromARGB(255, 203, 186, 0),
+              icon: Icons.star_purple500_outlined,
+              text: 'Marcadores favoritos',
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.pushNamed(context, 'map');
+            },
+            child: const _SigleCard(
+              color: Color.fromARGB(255, 203, 186, 0),
+              icon: Icons.hotel_rounded,
+              text: 'Mis reservas hotel',
+            ),
+          ),
+        ]),
       ],
     );
   }
 }
 
-
 class _SigleCard extends StatelessWidget {
-
   final IconData icon;
   final Color color;
   final String text;
 
-  const _SigleCard({
-    Key? key,
-    required this.icon,
-    required this.color,
-    required this.text
-  }) : super(key: key);
+  const _SigleCard(
+      {Key? key, required this.icon, required this.color, required this.text})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
-      return _CardBackground( 
+    return _CardBackground(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CircleAvatar(
-              backgroundColor: this.color,
-              child: Icon( this.icon, size: 35, color: Colors.white, ),
-              radius: 30,
-            ),
-            SizedBox( height: 10 ),
-            Text( this.text , style: TextStyle( color: this.color, fontSize: 18 ),)
-          ],
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        CircleAvatar(
+          backgroundColor: this.color,
+          child: Icon(
+            this.icon,
+            size: 35,
+            color: Colors.white,
+          ),
+          radius: 30,
+        ),
+        SizedBox(height: 10),
+        Text(
+          this.text,
+          style: TextStyle(color: this.color, fontSize: 18),
         )
-      );
+      ],
+    ));
   }
 }
 
-
 class _CardBackground extends StatelessWidget {
-
   final Widget child;
 
-  const _CardBackground({
-    Key? key, 
-    required this.child
-  }) : super(key: key);
+  const _CardBackground({Key? key, required this.child}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-          margin: EdgeInsets.all(15),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(20),
-            child: BackdropFilter(
-              filter: ImageFilter.blur( sigmaX: 5, sigmaY: 5 ),
-              child: Container(
-                height: 180,
-                decoration: BoxDecoration(
-                  color: const Color.fromRGBO(62, 66, 107, 0.7),
-                  borderRadius: BorderRadius.circular(20)
-                ),
-                child: this.child,
+      margin: EdgeInsets.all(15),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+          child: Container(
+            height: 180,
+            decoration: BoxDecoration(
+                color: const Color.fromRGBO(62, 66, 107, 0.7),
+                borderRadius: BorderRadius.circular(20)),
+            child: this.child,
           ),
         ),
       ),
