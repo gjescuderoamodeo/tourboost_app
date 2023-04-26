f17ad612f114ed7ade730e0c4a3daf4605e01f98
gjescuderoamodeo <49474070+gjescuderoamodeo@users.noreply.github.com>
2023-04-26 13:14:43 +0200

    Update lugares.dart

--- lib/models/lugares.dart
+++ lib/models/lugares.dart
@@ -4,6 +4,7 @@ class Lugar {
   final double longitud;
   final String tipoLugar;
   final String nombrePais;
+  final String nombre;
 
   Lugar({
     required this.idLugar,
@@ -11,6 +12,7 @@ class Lugar {
     required this.longitud,
     required this.tipoLugar,
     required this.nombrePais,
+    required this.nombre,
   });
 
   factory Lugar.fromJson(Map<String, dynamic> json) {
@@ -19,6 +21,7 @@ class Lugar {
       latitud: json['latitud'].toDouble(),
       longitud: json['longitud'].toDouble(),
       tipoLugar: json['tipo_lugar'],
+      nombre: json['nombre'],
       nombrePais: json['nombrePais'],
     );
   }
