09b372979d230b610b8ef9d6a370162248df38ac
gjescuderoamodeo <49474070+gjescuderoamodeo@users.noreply.github.com>
2023-05-06 14:21:55 +0200

    Update hotel_screen.dart

--- lib/screens/hotel_screen.dart
+++ lib/screens/hotel_screen.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:tourboost_app/models/models.dart';
 import 'package:tourboost_app/screens/screens.dart';
 import 'package:tourboost_app/services/services.dart';
+import 'package:intl/intl.dart';
+
+import 'package:fluttertoast/fluttertoast.dart';
 
 class HotelScreen extends StatefulWidget {
   const HotelScreen({super.key});
@@ -14,7 +17,7 @@ class HotelScreen extends StatefulWidget {
 
 class _HotelScreenState extends State<HotelScreen> {
   //rango fecha
-  DateTimeRange? _selectedRange;
+  DateTimeRange? _selectedRange = null;
 
 //cuadro dialogo elegir fecha
   Future<void> _presentDateRangePicker(BuildContext context) async {
@@ -25,10 +28,10 @@ class _HotelScreenState extends State<HotelScreen> {
     );
     final newDateRange = await showDateRangePicker(
       context: context,
-      saveText: "Reservar hotel",
+      saveText: "Elegir fecha",
       helpText: "Elige fecha inicio fin",
-      firstDate: DateTime(2020),
-      lastDate: DateTime(2030),
+      firstDate: DateTime(2023),
+      lastDate: DateTime(2024),
       initialDateRange: _selectedRange ?? initialDateRange,
     );
     if (newDateRange == null) {
@@ -39,7 +42,7 @@ class _HotelScreenState extends State<HotelScreen> {
     });
 
     //se ejecuta al darle a guardar
-    print('La fecha seleccionada es $_selectedRange');
+    //print('La fecha seleccionada es $_selectedRange');
   }
 //
 
@@ -121,11 +124,87 @@ class _HotelScreenState extends State<HotelScreen> {
             trailing: const Icon(Icons.add),
             onTap: () {
               //reservas alert
-              _presentDateRangePicker(context);
+              //_presentDateRangePicker(context);
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  int numeroReserva = 0;
+
+                  return AlertDialog(
+                    title: const Text('Crear Reserva'),
+                    content: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        TextField(
+                          keyboardType: TextInputType.number,
+                          onChanged: (value) {
+                            numeroReserva = int.parse(value);
+                          },
+                          decoration: const InputDecoration(
+                            labelText: 'Número a reservar',
+                          ),
+                        ),
+                        const SizedBox(height: 16),
+                        TextButton(
+                          child: const Text('Elegir fecha'),
+                          onPressed: () async {
+                            _presentDateRangePicker(context);
+                            setState(() {});
+                          },
+                        ),
+                        //no se actualiza el valor y queda feo
+                        /*const Text("fecha elegida:"),
+                        Text(
+                          _selectedRange != null
+                              ? '${DateFormat.yMMMMd().format(_selectedRange!.start)} - ${DateFormat.yMMMMd().format(_selectedRange!.end)}'
+                              : 'Ningún rango seleccionado',
+                        ),*/
+                      ],
+                    ),
+                    actions: [
+                      TextButton(
+                        child: const Text('Cancelar'),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                      TextButton(
+                        child: const Text('Crear'),
+                        onPressed: () {
+                          if (_selectedRange == null) {
+                            Fluttertoast.showToast(
+                                msg: "Seleccione una fecha",
+                                toastLength: Toast.LENGTH_SHORT,
+                                gravity: ToastGravity.CENTER,
+                                timeInSecForIosWeb: 3,
+                                backgroundColor:
+                                    const Color.fromARGB(255, 251, 0, 0),
+                                textColor: Colors.white,
+                                fontSize: 16.0);
+                          } else if (numeroReserva <= 0) {
+                            Fluttertoast.showToast(
+                                msg: "Seleccione una cantidad a reservar",
+                                toastLength: Toast.LENGTH_SHORT,
+                                gravity: ToastGravity.CENTER,
+                                timeInSecForIosWeb: 3,
+                                backgroundColor:
+                                    const Color.fromARGB(255, 251, 0, 0),
+                                textColor: Colors.white,
+                                fontSize: 16.0);
+                          } else {
+                            Navigator.of(context).pop();
+                          }
+                        },
+                      ),
+                    ],
+                  );
+                },
+              );
             },
             contentPadding:
                 const EdgeInsets.symmetric(vertical: 16, horizontal: 20),
           ),
+          //opcion ver en el mapa el hotel
           ListTile(
             title: const Text(
               'Ver en el Mapa',
