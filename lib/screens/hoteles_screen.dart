fed5a051b6c225a54a453253e2b02a1799f0c1f4
gjescuderoamodeo <49474070+gjescuderoamodeo@users.noreply.github.com>
2023-05-06 13:07:18 +0200

    Update hoteles_screen.dart
    hoteles recogidos de la api

--- lib/screens/hoteles_screen.dart
+++ lib/screens/hoteles_screen.dart
@@ -4,6 +4,10 @@ import 'package:tourboost_app/screens/screens.dart';
 import 'package:tourboost_app/services/services.dart';
 import 'package:tourboost_app/theme/app_theme.dart';
 
+import '../models/models.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
 class HotelesScreen extends StatefulWidget {
   const HotelesScreen({super.key});
 
@@ -15,14 +19,29 @@ class _HotelesScreenState extends State<HotelesScreen> {
   //variable menú desplegable
   SampleItem? selectedMenu;
 
-  final List<Map<String, dynamic>> data = [
-    {
-      "nombre": "hotel1",
-    },
-    {
-      "nombre": "hotel2",
+  //obtener hoteles api
+  List<Hotel> hoteles = [];
+
+  Future<void> _getHoteles() async {
+    final response =
+        await http.get(Uri.parse('https://tour-boost-api.vercel.app/hotel'));
+
+    //print(response.body);
+
+    if (response.statusCode == 200) {
+      final parsed = json.decode(response.body).cast<Map<String, dynamic>>();
+      hoteles = parsed.map<Hotel>((json) => Hotel.fromJson(json)).toList();
+      setState(() {});
+    } else {
+      throw Exception('Failed to load hoteles');
     }
-  ];
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getHoteles();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -88,12 +107,12 @@ class _HotelesScreenState extends State<HotelesScreen> {
       ),
       body: ListView(
         children: [
-          ...data
+          ...hoteles
               .map((e) => ListTile(
                   onTap: () {
                     Navigator.pushNamed(context, 'hotel', arguments: e);
                   },
-                  title: Text(e["nombre"]),
+                  title: Text(e.nombre),
                   trailing: const Icon(Icons.arrow_forward_ios_sharp)))
               .toList()
         ],
