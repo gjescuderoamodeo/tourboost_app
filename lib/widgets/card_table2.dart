bb63f30693aa8daef51a97978cd7a63bef5b2524
gjescuderoamodeo <49474070+gjescuderoamodeo@users.noreply.github.com>
2023-06-04 14:51:35 +0200

    Update card_table2.dart

--- lib/widgets/card_table2.dart
+++ lib/widgets/card_table2.dart
@@ -48,7 +48,7 @@ class CardTable2 extends StatelessWidget {
               Navigator.pushNamed(context, 'adminrecomendacion');
             },
             child: const _SigleCard(
-              color: Color.fromARGB(255, 119, 20, 130),
+              color: Color.fromARGB(255, 198, 73, 212),
               icon: Icons.event_available_rounded,
               text: 'Administración recomendación',
             ),
