2351e8fb305535356271509bc8a97b01a7ea635c
gjescuderoamodeo <49474070+gjescuderoamodeo@users.noreply.github.com>
2023-06-04 13:14:25 +0200

    Update configuraciones_screen.dart
    add delete user

--- lib/screens/configuraciones_screen.dart
+++ lib/screens/configuraciones_screen.dart
@@ -2,9 +2,57 @@ import 'package:flutter/material.dart';
 import 'package:tourboost_app/services/auth_service.dart';
 import 'package:tourboost_app/theme/app_theme.dart';
 import 'package:tourboost_app/widgets/widgets.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class ConfiguracionesScreen extends StatelessWidget {
+late int _idUser;
+//función obtener id usuario
+Future<void> _getUserId() async {
+  final AuthService authService = AuthService();
+
+  //obtener token
+  final token = await authService.readToken();
+  final userId = authService.getUserIdFromToken(token);
+
+  _idUser = userId;
+}
+
+//borrar usuario api
+Future<void> _delUsuario(context) async {
+  final body = {
+    'idUsuario': _idUser,
+  };
+
+  final response = await http.delete(
+    Uri.parse('https://tour-boost-api.vercel.app/usuario'),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(body),
+  );
+
+  print(response.statusCode);
+
+  if (response.statusCode == 200) {
+    Navigator.popAndPushNamed(context, 'login');
+  } else {
+    throw Exception('Failed to delete usuario');
+  }
+}
+
+class ConfiguracionesScreen extends StatefulWidget {
   const ConfiguracionesScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ConfiguracionesScreen> createState() => _ConfiguracionesScreenState();
+}
+
+class _ConfiguracionesScreenState extends State<ConfiguracionesScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _getUserId();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -96,7 +144,30 @@ class BorrarCuentaCard extends StatelessWidget {
               style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
         ),
         onTap: () {
-          // Acción cuando se toque la tarjeta "Borrar cuenta"
+          showDialog(
+            context: context,
+            builder: (_) => AlertDialog(
+              title: const Text('¿Desea borrar su cuenta?'),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Cancelar'),
+                ),
+                ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(AppTheme.alert),
+                  ),
+                  onPressed: () {
+                    _delUsuario(context);
+                  },
+                  child: const Text('Borrar cuenta'),
+                ),
+              ],
+            ),
+          );
         },
       ),
     );
